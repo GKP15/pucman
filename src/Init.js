a54c671dcb76753ad82be395fdf669a68ce328c8
KayVollers <kay_vollers@web.de>
2015-04-17 14:42:49 +0200

    Move Map Creation 2

--- src/Init.js
+++ src/Init.js
@@ -2,15 +2,6 @@
 init = function() {
 	//erstelle neues Spielobject. (Höhe in %, Breite in %, Renderer, HTML-Container, default state, transparent)
 	var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameContainer', null, true);
-	//Erstellt neue Karte. (HTML-Container, Map-API)
-	map = new mxn.Mapstraction('map', 'openlayers');
-	//Center-location
-	var latlon = new mxn.LatLonPoint(51.50733, -0.12769);
-	//set Center and zoomlevel
-	map.setCenterAndZoom(latlon, 10);
-	map.setOption('enableDragging', false);
-	//disable zoom panel
-	map.addControls('pan', false);
 	//add game states
 	game.state.add('Boot', Pucman.Boot);
 	game.state.add('Preloader', Pucman.Preloader);
