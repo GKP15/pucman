f2937ba44638f2a0c01a9dc51a0a0e0702dc4e74
Fatrago <FTronicke@gmx.de>
2015-04-17 14:39:45 +0200

    main menu#1

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -2,6 +2,7 @@ Pucman.MainMenu = function(game) {};
 
 Pucman.MainMenu.prototype = {
 	create: function() {
+		game.state.states['Boot'].map.addControls('pan', true);
 		//gehe in state Game
 		this.state.start('Game');
 	},
