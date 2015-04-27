06ede555e02f4efa4a36a77436c86c64330c7bda
beavis <gordian.dziwis@gmail.com>
2015-04-27 17:25:29 +0200

    Reduced to needed states

--- src/Init.js
+++ src/Init.js
@@ -2,14 +2,21 @@
  * Einstieg aus der HTML-Datei
  */
 init = function() {
-	//Höhe in %, Breite in %, Renderer, HTML-Container, default state, transparent)
-	/**	phaser-game */
-	var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameContainer', null, true);
-	//add game states
-	game.state.add('Boot', Pucman.Boot);
-	game.state.add('Preloader', Pucman.Preloader);
-	game.state.add('MainMenu', Pucman.MainMenu);
-	game.state.add('Game', Pucman.Game);
-	//start state Boot
-	game.state.start('Boot');
+    //Höhe in %, Breite in %, Renderer, HTML-Container, default state, transparent)
+    /**	phaser-game */
+    var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameContainer', null, true);
+    map = new mxn.Mapstraction('map', 'openlayers');
+    //Center-location
+    var latlon = new mxn.LatLonPoint(51.3365278, 12.3764688);
+    //set Center and zoomlevel
+    map.setCenterAndZoom(latlon, 10);
+    map.setOption('enableDragging', true);
+    //disable zoom panel
+    map.addControls('pan', true);
+
+    //add game states
+    game.state.add('MainMenu', Pucman.MainMenu);
+    game.state.add('Game', Pucman.Game);
+    //start state Boot
+    game.state.start('MainMenu', true, false, map);
 };
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,73 +1,72 @@
+var Pucman = {};
 Pucman.MainMenu = function(game) {};
 
 Pucman.MainMenu.prototype = {
-	
-	/**
-	 * initialisation of the game state and the geocoder
-	 */
-	init: function(map) {
-		var address = {};
-		address.address = 'Leipzig';
-		geocoder = new mxn.Geocoder('openlayers', function(location) {
 
-		    // display the map centered on a latitude and longitude + zoom-level
-		    map.setCenterAndZoom(location.point, 15);
-		    
-		    // create a marker positioned at a lat/lon 
-		    var geocode_marker = new mxn.Marker(location.point);
-		    var bubble = location.locality + ", " + location.region;
+    /**
+     * initialisation of the game state and the geocoder
+     */
+    init: function(map) {
+        var address = {};
+        address.address = 'Leipzig';
+        geocoder = new mxn.Geocoder('openlayers', function(location) {
 
-			// open the marker
-			geocode_marker.openBubble();
-		    geocode_marker.setInfoBubble(bubble);
+            // display the map centered on a latitude and longitude + zoom-level
+            map.setCenterAndZoom(location.point, 15);
 
-		    // display marker 
-		    map.addMarker(geocode_marker);
-		});
-		geocoder.geocode(address);
-	},
-	
-	/**
-	 * creation of the game state
-	 */
-	create: function() {
-		//this.input.mouse.mouseWheelCallback = this.mouseWheel;
-		//button to play
-		homepageButton = this.game.add.button((this.game.width / 2), (this.game.height / 2), 'playButtonPic', this.playButtonClicked, this);
-		homepageButton.anchor.setTo(0.5,0.5);
-		
-	},
-	
-	/**
-	 * preloads of the game state
-	 */
-	preload: function() {
-		this.load.image('playButtonPic', 'resources/playButton.png');
-	},
-	
-	/**
-	 * update of the game state
-	 */
-	update: function() {
-	},
-	
-	/**
-	 * catch mousewheel movement
-	 */
-	mouseWheel: function(event) {
-		//Ausgabe des Wertes des Mausrads
-		console.log(this.input.mouse.wheelDelta);
-		console.log(map.getZoom());
-		this.world.scale.x = map.getZoom() / 10.0; 
-		this.world.scale.y = map.getZoom() / 10.0; 
-		map.setZoom(map.getZoom() + this.input.mouse.wheelDelta);
-	},
-	
-	/**
-	 * catch mousewheel movement
-	 */
-	playButtonClicked: function() {
-		this.state.start('Game');
-	}
+            // create a marker positioned at a lat/lon 
+            var geocode_marker = new mxn.Marker(location.point);
+            var bubble = location.locality + ", " + location.region;
+
+            // open the marker
+            geocode_marker.openBubble();
+            geocode_marker.setInfoBubble(bubble);
+
+            // display marker 
+            map.addMarker(geocode_marker);
+        });
+        geocoder.geocode(address);
+    },
+
+    /**
+     * creation of the game state
+     */
+    create: function() {
+        //this.input.mouse.mouseWheelCallback = this.mouseWheel;
+        //button to play
+        homepageButton = this.game.add.button((this.game.width / 2), (this.game.height / 2), 'playButtonPic', this.playButtonClicked, this);
+        homepageButton.anchor.setTo(0.5, 0.5);
+
+    },
+
+    /**
+     * preloads of the game state
+     */
+    preload: function() {
+        this.load.image('playButtonPic', 'resources/playButton.png');
+    },
+
+    /**
+     * update of the game state
+     */
+    update: function() {},
+
+    /**
+     * catch mousewheel movement
+     */
+    mouseWheel: function(event) {
+        //Ausgabe des Wertes des Mausrads
+        console.log(this.input.mouse.wheelDelta);
+        console.log(map.getZoom());
+        this.world.scale.x = map.getZoom() / 10.0;
+        this.world.scale.y = map.getZoom() / 10.0;
+        map.setZoom(map.getZoom() + this.input.mouse.wheelDelta);
+    },
+
+    /**
+     * catch mousewheel movement
+     */
+    playButtonClicked: function() {
+        this.state.start('Game');
+    }
 };
-
