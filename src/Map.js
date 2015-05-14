f075509208abfee3b33980da7bbfb3140772ac60
beavis <gordian.dziwis@gmail.com>
2015-05-15 00:02:30 +0200

    Initial commit

--- src/Map.js
+++ src/Map.js
@@ -1,40 +1,61 @@
-/**
- * Format for map should be:
- * UP;RIGHT;DOWN;LEFT;X;Y
- * Node identifier is the row.
- * Example
- * ;;1;120;130;
- * ;;0;120;120;
- */
+Pucman.map = (function() {
 
-Map = fuction(game) {
-    this.phaser = Phaser;
-}
+    var connectPointList = function(pointList) {
+        for (i = 0; i < pointList.length - 1; i++) {
+            connectTwoPoints(pointList[i], pointList[i + 1]);
+        }
+    };
+      
+    var interpolatePointList = function(pointList) {
+        var pointListX = [];
+        var pointListY = [];
+        var step = 1 / spanOfPointList(pointList);
+        while (pointList.length !== 0) {
+            var point = pointList.shift();
+            pointListX.push(point.x);
+            pointListY.push(point.y);
+        }
+        for (i = 0; i <= 1; i += step) {
+            var pX = Phaser.Math.catmullRomInterpolation(pointListX, i);
+            var pY = Phaser.Math.catmullRomInterpolation(pointListY, i);
+            pointList.push(new Phaser.Point(pX, pY));
+        }
+    };
 
-create = function() {
-    
+    var spanOfPointList = function(pointList) {
+        var span = 0;
+        for (var i = 0; i < pointList.length - 1; i++) {
+            span += Phaser.Point.distance(pointList[i], pointList[i + 1]);
+        }
+        return span;
+    };
 
-getNodeInDirection = function(entity, direction, graph) {
-    switch (direction) {
-        case UP:
-            return graph[entity.node][0];
-        case RIGHT:
-            return graph[entity.node][1];
-        case LEFT:
-            return graph[entity.node][2];
-        case DOWN:
-            return graph[entity.node][3];
-        default:
-            return entity.node;
-    }
-};
-Pacman.prototype.edge = null;
-var graph = [][];
+    var connectTwoPoints = function(pointA, pointB) {
+        var dir = Phaser.Point.subtract(pointA,  pointB);
+        if (dir.y < (-1 * Math.abs(dir.x))) {
+            pointA.up = pointB;
+            pointB.down = pointA;
+        } else if (dir.x > Math.abs(dir.y)) {
+            pointA.right = pointB;
+            pointB.left = pointA;
+        } else if (dir.y > Math.abs(dir.x)) {
+            pointA.down = pointB;
+            pointB.up = pointA;
+        } else if (dir.x < (-1 * Math.abs(dir.y))) {
+            pointA.left = pointB;
+            pointB.right = pointA;
+        }
+    };
 
-createGraph = function(nodelist) {
-    for ( int i = 0; i <= nodelist.length; i++ ) {
-       for ( int j = 0; j <= nodelist[i].length; j++ ) {
-          graph += interpolatePoints(nodelist[i][0], nodelist[i][j]);
-       }
-    }
-}
+    return {
+        createGraph : function(pointList) {
+            interpolatePointList(pointList);
+            connectPointList(pointList);
+        },
+        test : function(game) {
+            game.arsch = null;
+        }
+    };
+})();
+
+
